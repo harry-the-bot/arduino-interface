63eef1f606d968f07d583705463f080fc0cbbe0e
Danilo <fixmyprinter@danilorechi.net>
2016-10-02 23:21:31 -0300

    Process Improvements

--- main.c
+++ main.c
@@ -2,151 +2,158 @@ bool isMovingForward = false;
 bool isMovingBackward = false;
 bool isMovingLeft = false;
 bool isMovingRight = false;
+bool saidHello = false;
 
 void setup() {
 
-    pinMode(22, OUTPUT);
-    pinMode(23, OUTPUT);
-    pinMode(24, OUTPUT);
-    pinMode(25, OUTPUT);
+  Serial.begin(9600);
 
-    Serial.begin(9600);
-
+  pinMode(22, OUTPUT);
+  pinMode(23, OUTPUT);
+  pinMode(24, OUTPUT);
+  pinMode(25, OUTPUT);
 }
 
 void loop() {
 
-    String str = readSerial();
+  if (!saidHello) {
+    Serial.print("HEL");
+    saidHello = true;
+  }
 
-    if(str.length() > 0){
+  String str = readSerial();
 
-       if(str.charAt(0) == 'M'){
-          setCurrentMovement(str.charAt(1),100);
-       }
-
+  if (str.length() > 1) {
+    Serial.print(str);
+    if (str.charAt(0) == 'M') {
+    setCurrentMovement(str.charAt(1), 100);
     }
 
-    keepMoving();
+  }
 
+  keepMoving();
 
 }
 
-String readSerial(){
+String readSerial() {
 
-    String str;
+  String str = "";
 
-    while(Serial.available() > 0) {
-       delay(1);
-       char c = Serial.read();
-       if(c == '\n')
-          break;
-       str += c;
-    }
+  while (Serial.available() > 0) {
+    delay(1);
+    char c = (char) Serial.read();
 
-    if(str.length() > 0){
+    if (c == ';')
+    break;
 
-       return str;
+    str += c;
 
-    }
+  }
 
-    return "";
+  if (str.length() > 0) {
+
+    return str;
+
+  }
+
+  return "";
 
 }
 
-void keepMoving(){
-    if(isMovingForward){
-       showNumber(1);
-    }
+void keepMoving() {
+  if (isMovingForward) {
+    showNumber(1);
+  }
 
-    if(isMovingBackward){
-       showNumber(2);
-    }
+  if (isMovingBackward) {
+    showNumber(2);
+  }
 
-    if(isMovingLeft){
-       showNumber(3);
-    }
+  if (isMovingLeft) {
+    showNumber(3);
+  }
 
-    if(isMovingRight){
-       showNumber(4);
-    }
+  if (isMovingRight) {
+    showNumber(4);
+  }
 
-    if( !isMovingForward &&
-          !isMovingBackward &&
-          !isMovingLeft &&
-          !isMovingRight )
-       showNumber(7);
+  if (!isMovingForward &&
+    !isMovingBackward &&
+    !isMovingLeft &&
+    !isMovingRight)
+    showNumber(7);
 }
 
-void setCurrentMovement(char direction, int speed){
+void setCurrentMovement(char direction, int speed) {
 
+  isMovingForward = false;
+  isMovingBackward = false;
+  isMovingLeft = false;
+  isMovingRight = false;
+
+  switch (direction) {
+  case 'F':
+    isMovingForward = true;
+    break;
+  case 'B':
+    isMovingBackward = true;
+    break;
+  case 'L':
+    isMovingLeft = true;
+    break;
+  case 'R':
+    isMovingRight = true;
+    break;
+  default:
     isMovingForward = false;
     isMovingBackward = false;
     isMovingLeft = false;
     isMovingRight = false;
 
-    switch(direction){
-       case 'F':
-          isMovingForward = true;
-          break;
-       case 'B':
-          isMovingBackward = true;
-          break;
-       case 'L':
-          isMovingLeft = true;
-          break;
-       case 'R':
-          isMovingRight = true;
-          break;
-       default:
-          isMovingForward = false;
-          isMovingBackward = false;
-          isMovingLeft = false;
-          isMovingRight = false;
+  }
+
+}
+
+void turnAllOff() {
+  digitalWrite(22, LOW);
+  digitalWrite(23, LOW);
+  digitalWrite(24, LOW);
+  digitalWrite(25, LOW);
+}
+
+void showNumber(int number) {
+    turnAllOff();
+    switch (number) {
+    case 0:
+      turnAllOff();
+      break;
+    case 1:
+      digitalWrite(22, HIGH);
+      break;
+    case 2:
+      digitalWrite(23, HIGH);
+      break;
+    case 3:
+      digitalWrite(22, HIGH);
+      digitalWrite(23, HIGH);
+      break;
+    case 4:
+      digitalWrite(24, HIGH);
+      break;
+      break;
+    case 5:
+      digitalWrite(24, HIGH);
+      digitalWrite(22, HIGH);
+      break;
+    case 6:
+      digitalWrite(24, HIGH);
+      digitalWrite(23, HIGH);
+      break;
+    default:
+      digitalWrite(22, HIGH);
+      digitalWrite(23, HIGH);
+      digitalWrite(24, HIGH);
 
     }
-
-}
-
-void turnAllOff(){
-    digitalWrite(22, LOW);
-    digitalWrite(23, LOW);
-    digitalWrite(24, LOW);
-    digitalWrite(25, LOW);
-}
-
-void showNumber(int number){
-       turnAllOff();
-       switch(number){
-           case 0:
-             turnAllOff();
-             break;
-           case 1:
-             digitalWrite(22, HIGH);
-             break;
-           case 2:
-             digitalWrite(23, HIGH);
-             break;
-           case 3:
-             digitalWrite(22, HIGH);
-             digitalWrite(23, HIGH);
-             break;
-           case 4:
-             digitalWrite(24, HIGH);
-             break;
-             break;
-           case 5:
-             digitalWrite(24, HIGH);
-             digitalWrite(22, HIGH);
-             break;
-           case 6:
-             digitalWrite(24, HIGH);
-             digitalWrite(23, HIGH);
-             break;
-           default:
-             digitalWrite(22, HIGH);
-             digitalWrite(23, HIGH);
-             digitalWrite(24, HIGH);
-
-        }
 
 }
